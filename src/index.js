45e154bcb66d9b5f22a413eeff8235acd6c5fb62
mohammaddargahi <mohammaddargahi4@google.com>
2023-08-08 18:12:53 +0330

    handle server errors, http errors, and SIGTERM

--- src/index.js
+++ src/index.js
@@ -6,7 +6,42 @@ import logger from "./configs/logger.config.js";
 // env variables
 const PORT = process.env.PORT || 9000;
 
+let server;
 
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
+  console.log("process id", process.pid);
 });
+
+
+
+// handle server errors
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Shutting down the server...");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+  }
+
+  const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+  }
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+
+  // SIGTERM signal
+  process.on("SIGTERM", () => {
+    if (server) {
+      logger.info("SIGTERM received, shutting down gracefully");
+      server.close(() => {
+        logger.info("Process terminated");
+        process.exit(0);
+      });
+    }
+    
+  })
