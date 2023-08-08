d699844bff495f396adec15b540da4561b902918
mohammaddargahi <mohammaddargahi4@google.com>
2023-08-08 18:29:04 +0330

    handling not found error and bd request errors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
 
 // dotEnv config
 dotenv.config();
@@ -47,7 +48,22 @@ app.use(fileUpload({
 app.use(cors());
 
 app.post("/test", (req, res) => {
-  res.send(req.body);
+  throw createHttpError.BadRequest("This route has not been implemented yet");
 });
+
+app.use((err, req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist"));
+})
+
+// error handler
+app.use(async (err, req, res, next) => {
+   res.status(err.status || 500);
+   res.send({
+    error :{
+      status: err.status || 500,
+      message: err.message,
+    }
+   })
+})
 
 export default app;
